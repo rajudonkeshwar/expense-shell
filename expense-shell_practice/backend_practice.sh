9d5b4a23c24e7b592ab8c130e4cbad7833451744
Raju donkeshwar <dvijaychandrakumar@gmail.com>
2025-01-15 18:59:01 +0530

    i added expense folder

--- expense-shell_practice/backend_practice.sh
+++ expense-shell_practice/backend_practice.sh
@@ -11,7 +11,7 @@ LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-mkdir -p LOGS_FOLDER
+mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]
